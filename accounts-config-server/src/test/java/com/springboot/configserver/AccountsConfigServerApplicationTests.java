6bd12d5ebbdd28c14d61bf8a77ef4db7e52e3b31
KaushalNijhawan <kaushalnijhawan3641@gmail.com>
2023-03-20 13:50:40 +0000

    init commit for config-server

---
+++ accounts-config-server/src/test/java/com/springboot/configserver/AccountsConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
